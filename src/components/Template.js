6f9f57e26f79db21cfc48032d75f274454b18f25
Vonkez <vonkez21@gmail.com>
2021-04-18 23:08:27 +0300

    update template indicator

--- src/components/Template.js
+++ src/components/Template.js
@@ -1,50 +1,12 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCheckCircle } from "@fortawesome/free-solid-svg-icons";
-
-
 
 export default function Template({imgUrl, templateName, selected, select, id}) {
     return (
         <div 
         className={
             `transition duration-500 transform hover:-translate-y-2 hover:shadow-2xl 
-            rounded-xl mt-6 mb-6 mr-6 bg-gray-700 inline-block ${selected ? "ring-2 ring-green-600":""} `}
+            rounded-xl mt-6 mb-6 mr-6 bg-gray-700 inline-block ${selected ? "ring-4 ring-green-600":""} `}
          onClick={() => {select(id)}}
          >
             <div className='w-96 h-96 rounded-t-xl border-2 border-gray-700'>
