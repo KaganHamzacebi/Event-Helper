e0d5c990a38d8de666fdeb1c9af0a8c80be60406
Vonkez <vonkez21@gmail.com>
2021-04-20 12:48:58 +0300

    fix tailwind.config.js, complete event_createsuccess page

--- src/pages/EventCreate/EventCreateSuccess.js
+++ src/pages/EventCreate/EventCreateSuccess.js
@@ -1,15 +1,29 @@
-import Header from "../../components/Header"
+import Header from "../../components/Header";
+import { useEffect, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCheckCircle } from "@fortawesome/free-solid-svg-icons";
 
 export default function InvalidToken(props) {
-   
+    const [isAnimationPlayed, setIsAnimationPlayed] = useState(false)
+    useEffect(() => {
+        setTimeout(()=>{setIsAnimationPlayed(true)}, 200)
+    }, [])
     return (
         <>
-        <Header></Header>
-        <div className='w-full h-full flex items-center justify-center'>
-            <h3 className="text-2xl font-bold ">Test 1</h3>
-            
-        </div>
+            <Header></Header>
+
+            <div className='w-full h-full flex items-center justify-center'>    
+                <div className="flex flex-col justify-center flex-wrap container mx-5 md:mx-40 xl:mx-96 text-center bg-title p-12 mb-28 rounded-xl shadow-lg">
+                    <FontAwesomeIcon className={`mx-auto my-auto transform transition-transform duration-1000 scale-0 ${isAnimationPlayed ? "scale-100":"scale-0"}`}  color="green" icon={faCheckCircle} size="6x"></FontAwesomeIcon>
+                    <div className="m-5 ">
+                        <div className="text-3xl xl:text-5xl text-primary font-bold mb-1">Event Created!</div>
+                        <div className="text-3xl xl:text-5xl text-primary font-light">You can close this window now</div>
+                    </div>
+
+                </div>
+
+            </div>
         </>
-       
+
     );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,28 +4,28 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: 'class', // or 'media' or 'class'
   theme: {
-    backgroundColor: theme => ({
-     ...theme('colors'),
-     'header': '#292b2f',
-     'title': '#2f3136',
-     'content': '#36393f',
-     'primary': '#6366F1',
-     'primary-light': '#7F82F2',
-     'white': '#FFFFFF',
-     'danger': '#e3342f'
-    }),
-    textColor: theme => theme('colors'),
-     textColor: {
-       'primary': '#FFFFFF',
-       'primary-light': '#8D8F91',
-       'danger': '#e3342f'
-     },
-     placeholderColor: {
-      'primary': '#36393f',
-    },
-    divideColor: {
-      'primary': '#2f3136'
-    }
+    extend: {
+      backgroundColor: {
+        'header': '#292b2f',
+        'title': '#2f3136',
+        'content': '#36393f',
+        'primary': '#6366F1',
+        'primary-light': '#7F82F2',
+        'white': '#FFFFFF',
+        'danger': '#e3342f'
+      },
+      textColor: {
+        'primary': '#FFFFFF',
+        'primary-light': '#8D8F91',
+        'danger': '#e3342f',
+      },
+      placeholderColor: {
+        'primary': '#ffffff',
+      },
+      divideColor: {
+        'primary': '#2f3136'
+      }
+    }   
   },
   variants: {
     extend: {
