bf3246fa6c2db2661f6fbf3545535e1c93c6e2ca
KaganHamzacebi <kagan.hcebi@gmail.com>
2021-05-21 03:36:06 +0300

    react-snap update

--- src/index.js
+++ src/index.js
@@ -2,29 +2,47 @@ import './index.css';
 import App from './App';
 import React from 'react';
 import ReactGA from 'react-ga';
-import ReactDOM from 'react-dom';
 import TagManager from 'react-gtm-module'
+import { hydrate, render } from "react-dom";
 import { CookiesProvider } from 'react-cookie';
 import reportWebVitals from './reportWebVitals';
 import { HelmetProvider } from 'react-helmet-async';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactGA.initialize('UA-197548337-1');
-TagManager.initialize('GTM-NJSM4BL');
+TagManager.initialize({ gtmId: 'GTM-NJSM4BL' });
 ReactGA.pageview(window.location.pathname + window.location.search);
 
-ReactDOM.render(
-  <React.StrictMode>
-    <HelmetProvider>
-      <CookiesProvider>
-        <Router>
-          <App />
-        </Router>
-      </CookiesProvider>
-    </HelmetProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const root = document.getElementById("root");
+
+if (root.hasChildNodes()) {
+  hydrate(
+    <React.StrictMode>
+      <HelmetProvider>
+        <CookiesProvider>
+          <Router>
+            <App />
+          </Router>
+        </CookiesProvider>
+      </HelmetProvider>
+    </React.StrictMode>,
+    root
+  );
+}
+else {
+  render(
+    <React.StrictMode>
+      <HelmetProvider>
+        <CookiesProvider>
+          <Router>
+            <App />
+          </Router>
+        </CookiesProvider>
+      </HelmetProvider>
+    </React.StrictMode>,
+    root
+  );
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
