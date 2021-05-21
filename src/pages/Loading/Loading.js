049cae4e463106d2a65c20d2cfb19fd09cb89625
KaganHamzacebi <kagan.hcebi@gmail.com>
2021-05-22 01:08:26 +0300

    ui fix

--- src/pages/Loading/Loading.js
+++ src/pages/Loading/Loading.js
@@ -3,8 +3,8 @@ import './Loading.css';
 export default function Loading() {
 
     return (
-        <div>
-            <div className="loader">
+        <div className='flex'>
+            <div className="m-auto loader">
                 <div className="bubble"></div>
                 <div className="bubble"></div>
                 <div className="bubble"></div>
