54cdeeebcc7b623bc90b2cff07ff7681e52ed622
KaganHamzacebi <kagan.hcebi@gmail.com>
2021-05-25 05:38:02 +0300

    hydrate removed

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import App from './App';
 import React from 'react';
 import ReactGA from 'react-ga';
 import TagManager from 'react-gtm-module'
-import { hydrate, render } from "react-dom";
+import ReactDOM from 'react-dom';
 import { CookiesProvider } from 'react-cookie';
 import reportWebVitals from './reportWebVitals';
 import { HelmetProvider } from 'react-helmet-async';
@@ -13,36 +13,18 @@ ReactGA.initialize('UA-197548337-1');
 TagManager.initialize({ gtmId: 'GTM-NJSM4BL' });
 ReactGA.pageview(window.location.pathname + window.location.search);
 
-const root = document.getElementById("root");
-
-if (root.hasChildNodes()) {
-  hydrate(
-    <React.StrictMode>
-      <HelmetProvider>
-        <CookiesProvider>
-          <Router>
-            <App />
-          </Router>
-        </CookiesProvider>
-      </HelmetProvider>
-    </React.StrictMode>,
-    root
-  );
-}
-else {
-  render(
-    <React.StrictMode>
-      <HelmetProvider>
-        <CookiesProvider>
-          <Router>
-            <App />
-          </Router>
-        </CookiesProvider>
-      </HelmetProvider>
-    </React.StrictMode>,
-    root
-  );
-}
+ReactDOM.render(
+  <React.StrictMode>
+    <HelmetProvider>
+      <CookiesProvider>
+        <Router>
+          <App />
+        </Router>
+      </CookiesProvider>
+    </HelmetProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
