3485bb38bda8e566ca9626b33e206a3cdd3d4a21
Kağan Hamzaçebi <kagan.hcebi@gmail.com>
2021-10-03 04:25:17 +0300

    Changes by lil

--- src/components/Switch/Switch.js
+++ src/components/Switch/Switch.js
@@ -64,64 +64,68 @@ export default function Switch({
         setColor(color.hex);
     };
 
-  return (
-    <div className="flex items-center justify-end w-full">
-      {
-        type === 'switch' ?
-          <label className="flex items-center cursor-pointer">
-            <div className="relative">
-              <input type="checkbox" id="toggle" name={name} value={enabled} onChange={(e) => setEnabled(e.target.checked)} checked={enabled} className="sr-only" />
-              <div className={`block w-14 h-8 rounded-full shadow-xl ${enabled ? 'bg-green-500' : 'bg-gray-600'} `}></div>
-              <div className="dot absolute left-1 top-1 bg-white w-6 h-6 rounded-full transition"></div>
-            </div>
-          </label>
-          : type === 'text' ?
-            <div className="flex items-center justify-end">
-              <input
-                type='text'
-                id="switch-text"
-                name={name}
-                placeholder={placeholder}
-                value={valueText}
-                onChange={(e) => setValueText(e.target.value)}
-                className="px-3 py-1 bg-title text-primary relative rounded text-lg border border-gray-900 outline-none 
-             focus:ring-2 focus:ring-blue-600 w-4/5" />
-            </div>
-            : type === 'number' ?
-              <div className="flex items-center justify-end">
-                <input
-                  type='number'
-                  min={0}
-                  id="switch-text"
-                  name={name}
-                  placeholder={placeholder}
-                  value={valueNumber}
-                  onChange={(e) => setValueNumber(e.target.value)}
-                  className="px-3 py-1 bg-title text-primary relative rounded text-lg border border-gray-900 outline-none 
-             focus:ring-2 focus:ring-blue-600 w-3/5" />
-              </div>
-              : type === 'select' ?
-                <div>
-                  <SelectInput name={name} content={content} placeholder="Select" width={32} />
-                </div>
-                : type === 'color-picker' ?
-                  <div>
-                    <input type="hidden" name={name} value={color} />
-                    <div style={colorPickerStyles.swatch} onClick={handleClick}>
-                      <div style={colorPickerStyles.color} />
-                    </div>
-                    {displayColorPicker ?
-                      <div style={colorPickerStyles.popover}>
-                        <div style={colorPickerStyles.cover} onClick={handleClose} />
-                        <SketchPicker name={name} value={color} color={color} onChange={handleChange} disableAlpha />
-                      </div>
-                      :
-                      null
-                    }
-                  </div>
-                  :
-                  ''
-      }
+    return (
+        <div className="flex items-center justify-end w-full">
+            {
+                type === 'switch' ?
+                    <label className="flex items-center cursor-pointer">
+                        <div className="relative">
+                            <input type="checkbox" id="toggle" name={name} value={enabled}
+                                   onChange={(e) => setEnabled(e.target.checked)} checked={enabled}
+                                   className="sr-only"/>
+                            <div
+                                className={`block w-14 h-8 rounded-full shadow-xl ${enabled ? 'bg-green-500' : 'bg-gray-600'} `}></div>
+                            <div className="dot absolute left-1 top-1 bg-white w-6 h-6 rounded-full transition"></div>
+                        </div>
+                    </label>
+                    : type === 'text' ?
+                        <div className="flex items-center justify-end">
+                            <input
+                                type='text'
+                                id="switch-text"
+                                name={name}
+                                placeholder={placeholder}
+                                value={valueText}
+                                onChange={(e) => setValueText(e.target.value)}
+                                className="px-3 py-1 bg-title text-primary relative rounded text-lg border border-gray-900 outline-none
+             focus:ring-2 focus:ring-blue-600 w-4/5"/>
+                        </div>
+                        : type === 'number' ?
+                            <div className="flex items-center justify-end">
+                                <input
+                                    type='number'
+                                    min={0}
+                                    id="switch-text"
+                                    name={name}
+                                    placeholder={placeholder}
+                                    value={valueNumber}
+                                    onChange={(e) => setValueNumber(e.target.value)}
+                                    className="px-3 py-1 bg-title text-primary relative rounded text-lg border border-gray-900 outline-none
+             focus:ring-2 focus:ring-blue-600 w-3/5"/>
+                            </div>
+                            : type === 'select' ?
+                                <div>
+                                    <SelectInput name={name} content={content} placeholder="Select" width={32}/>
+                                </div>
+                                : type === 'color-picker' ?
+                                    <div>
+                                        <input type="hidden" name={name} value={color}/>
+                                        <div style={colorPickerStyles.swatch} onClick={handleClick}>
+                                            <div style={colorPickerStyles.color}/>
+                                        </div>
+                                        {displayColorPicker ?
+                                            <div style={colorPickerStyles.popover}>
+                                                <div style={colorPickerStyles.cover} onClick={handleClose}/>
+                                                <SketchPicker name={name} value={color} color={color}
+                                                              onChange={handleChange} disableAlpha/>
+                                            </div>
+                                            :
+                                            null
+                                        }
+                                    </div>
+                                    :
+                                    ''
+            }
 
         </div>
     );
